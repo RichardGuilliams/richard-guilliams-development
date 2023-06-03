1a4bf89e4b620cac0e3a8568c97676b3b1948c4d
Richard Guilliams <dropletswebdevelopment@gmail.com>
2023-06-03 12:19:10 -0600

    Fixed import statements

--- pages/skills.js
+++ pages/skills.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/header'
-import Footer from './components/footer'
+import Header from '../components/header'
+import Footer from '../components/footer'
 import '../styles/styles.css';
 
 function Skills(){
