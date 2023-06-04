f2823d8c471cf6dd844426e28eba64054e927619
Richard Guilliams <dropletswebdevelopment@gmail.com>
2023-06-04 16:04:57 -0600

    Complete Site v1

--- pages/about.js
+++ pages/about.js
@@ -26,6 +26,7 @@ function AboutSection({ isActive }) {
       <div>
         <h2>About Me</h2>
         <p>Hello, I&apos;m Richard, a web developer with a deep passion for gaming, music, and movies. I have honed my programming skills through various platforms such as Udemy, YouTube, and resources like W3Schools and Stack Overflow. My dream is to earn my living working from home, using the skills I have gained to advance my career while enjoying the freedom of working remotely.</p>
+        <p>My passion for gaming led me to becoming interested in programming because deep down I&apos;ve always wanted to make games for the world to play. Already owning a copy of RPG Maker, I took up online Javascript classes and through those my love for web development grew as well.</p>
       </div>
     </div>
   )
